9dbf4c36ccab10bb0162311553d6971d1c36bd6e
Jamie McLaughlin <jamie.mclaughlin@mcdermottgroup.co.uk>
2021-01-01 12:42:22 +0000

    Added secondary task and push mechanics.

--- TimeTracker/app.R
+++ TimeTracker/app.R
@@ -48,6 +48,8 @@ ui <- navbarPage(
                         width = 8,
                         h2("Enter Tasks", align = "center"),
                         hr(),
+                        h3("Main Task", align = "center"),
+                        hr(),
                         fluidRow(
                             column(
                                 width = 4,
@@ -96,6 +98,58 @@ ui <- navbarPage(
                                 )
                             )
                         ),
+                        hr(),
+                        h3("Secondary Task", align = "center"),
+                        hr(),
+                        fluidRow(
+                            column(
+                                width = 4,
+                                dateInput(
+                                    "sub_date2",
+                                    label = "Activity date:",
+                                    value = today(),
+                                    weekstart = 1
+                                )
+                            ),
+                            column(
+                                width = 4,
+                                textInput(
+                                    "sub_start2",
+                                    label = "Start Time:"
+                                )
+                            ),
+                            column(
+                                width = 4,
+                                textInput(
+                                    "sub_end2",
+                                    label = "End Time:"
+                                )
+                            )
+                        ),
+                        fluidRow(
+                            column(
+                                width = 4,
+                                selectInput(
+                                    "sub_group2",
+                                    label = "Task Group:",
+                                    choices = c(task_groups),
+                                    selected = "",
+                                    multiple = F
+                                )
+                            ),
+                            column(
+                                width = 4,
+                                uiOutput("sub_subgroup2")
+                            ),
+                            column(
+                                width = 4,
+                                textInput(
+                                    "sub_notes2",
+                                    label = "Additional Notes:"
+                                )
+                            )
+                        ),
+                        hr(),
                         fluidRow(
                             column(
                                 width = 4,
@@ -135,6 +189,13 @@ server <- function(input, output, session) {
         updateSelectInput(session, "sub_group", selected = "")
         updateSelectInput(session, "sub_subgroup", selected = "")
         updateTextInput(session, "sub_notes", value = "")
+        
+        updateDateInput(session, "sub_date2", value = today())
+        updateTextInput(session, "sub_start2", value = "")
+        updateTextInput(session, "sub_end2", value = "")
+        updateSelectInput(session, "sub_group2", selected = "")
+        updateSelectInput(session, "sub_subgroup2", selected = "")
+        updateTextInput(session, "sub_notes2", value = "")
         
     }
 
@@ -180,6 +241,48 @@ server <- function(input, output, session) {
         
     })
     
+    output$sub_subgroup2 <- renderUI({
+        
+        if (input$sub_group2 == "Family") {
+            
+            subgroup2 <- c("Logan", "Ash", "House", "Meals", "Walks", "Family Finance", "Improvements", "Other")
+            
+        } else if (input$sub_group2 == "Health") {
+            
+            subgroup2 <- c("Exercise", "Reading", "Language", "Other")
+            
+        } else if (input$sub_group2 == "Professional") {
+            
+            subgroup2 <- c("Coursera", "Datacamp", "New Role Effort", "Personal Website", "Blog and Social Posts", "NFL GM Game Project", "Other Projects", "Work", "Other")
+            
+        } else if (input$sub_group2 == "Skills") {
+            
+            subgroup2 <- c("Room Decoration", "Garden Work", "Woodwork and Furniture", "Other")
+            
+        } else if (input$sub_group2 == "Hobby") {
+            
+            subgroup2 <- c("PC Construction", "Guitar Learning", "Piano Learning", "Guitar Construction", "Pedal Construction", "Other")
+            
+        } else if (input$sub_group2 == "Waste") {
+            
+            subgroup2 <- c("Transit", "Social", "Gaming", "Other")
+            
+        } else {
+            
+            subgroup2 <- c("")
+            
+        }
+        
+        selectInput(
+            "sub_subgroup2",
+            label = "SubGroup:",
+            choices = c(subgroup2, ""),
+            selected = "",
+            multiple = F
+        )
+        
+    })
+    
     observeEvent(input$sub_go_task, {
         
         data <- data.frame(
@@ -190,6 +293,41 @@ server <- function(input, output, session) {
             ,TaskSubGroup = input$sub_subgroup
             ,Notes = input$sub_notes
         )
+        
+        validate2 <- (
+            !is.na(input$sub_start2)
+            & !is.na(input$sub_group2)
+            & input$sub_start2 != ""
+            & input$sub_group2 != ""
+        )
+        
+        if (validate2 == T) {
+            
+            data2 <- data.frame(
+                TaskDate = input$sub_date2
+                ,StartTime = input$sub_start2
+                ,EndTime = input$sub_end2
+                ,TaskGroup = input$sub_group2
+                ,TaskSubGroup = input$sub_subgroup2
+                ,Notes = input$sub_notes2
+                ,TaskLevel = "Secondary"
+            )
+            
+            con_tsk <- dbConnect(
+                odbc::odbc(),
+                .connection_string = conf$con_str,
+                timeout = 5
+            )
+            
+            dbAppendTable(
+                conn = con_tsk,
+                name = SQL(conf$table),
+                value = data2
+            )
+            
+            dbDisconnect(con_tsk)
+            
+        }
         
         con_tsk <- dbConnect(
             odbc::odbc(),
